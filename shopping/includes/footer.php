fe88709846928e75c1084ec4a6ebc2187483e0c6
Ayush Kumar <ayush.kumar1913111014@gmail.com>
2022-04-17 12:15:03 +0530

    Hello Guys this is AYUSH

---
+++ shopping/includes/footer.php
@@ -0,0 +1,132 @@
+<footer id="footer" class="footer color-bg">
+    <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= CONTACT INFO ============================================================= -->
+                    <div class="contact-info">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <a href="index.php">
+
+                                    <!-- <h3 style="color: #fff;">Shopping Portal</h3> -->
+                                    <img style="    margin-top: -8px;padding-bottom: 6px;height: 40px" src="./brandsimage/logo-white.png" alt="gdhjk">
+                                    <li><a href="https://urbanstalls.com/sellerlogin/"><i class="icon fa fa-sign-in"></i>&nbsp Seller Login</a></li>
+                                    <li><a href="https://urbanstalls.com/admin/"><i class="icon fa fa-sign-in"></i>&nbsp R&D Team Login</a></li>
+                                    <li><a href="https://urbanstalls.com/admin/courier/"><i class="icon fa fa-sign-in"></i>&nbsp Courier Service Login</a></li>
+                                    <li><a href="https://urbanstalls.com/sellerdetail.php"><i class="icon fa fa-sign-in"></i>&nbsp Feedback Seller</a></li>
+                                </a>
+                            </div><!-- /.logo -->
+
+                        </div><!-- /.footer-logo -->
+
+                        <div class="module-body m-t-20">
+                            <p class="about-us" style="color: #fff;"> Brought To You By <a href="localhost/shopping/">shopping portal </a></p>
+                            <div class="social-icons">
+
+                                <a href="localhost/shopping/" class='active'><i class="icon fa fa-facebook"></i></a>
+                                <a href="###"><i class="icon fa fa-twitter"></i></a>
+                                <a href="##"><i class="icon fa fa-linkedin"></i></a>
+                                <a href="###"><i class="icon fa fa-rss"></i></a>
+                                <a href="##"><i class="icon fa fa-pinterest"></i></a>
+
+                            </div><!-- /.social-icons -->
+                        </div><!-- /.module-body -->
+
+                    </div><!-- /.contact-info -->
+                    <!-- ============================================================= CONTACT INFO : END ============================================================= -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= CONTACT TIMING============================================================= -->
+                    <div class="contact-timing">
+                        <div class="module-heading">
+                            <h4 class="module-title" style="color: #fff;">Delivery Times</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <td style="color: #fff;">Monday-Friday:</td>
+                                            <td class="pull-right" style="color: #fff;">10:00AM To 06:00PM</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="color: #fff;">Saturday:</td>
+                                            <td class="pull-right" style="color: #fff;">11:00AM To 06:00PM</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="color: #fff;">Sunday:</td>
+                                            <td class="pull-right" style="color: #fff;">12:00PM To 06:00PM</td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                                <br>
+                                <br>
+                                <center>
+                                    <p style="color:#fff;">Brought To You By & Copyright © 2022 <a href="https://redstore1424.wixsite.com/shopcom">Red_store</a></p>
+
+                                </center>
+                            </div><!-- /.table-responsive -->
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+                    <!-- ============================================================= CONTACT TIMING : END ============================================================= -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= INFORMATION============================================================= -->
+                    <div class="contact-information">
+                        <div class="module-heading">
+                            <h4 class="module-title" style="color: #fff;">information</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <ul class="toggle-footer">
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p style="color: #fff;">INDIA</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p style="color: #fff;">(011) 3456789099<br>(011) 2345678988</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <span><a href="https://urbanstalls.com/">urbanstalls.com</a></span>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div><!-- /.module-body -->
+
+                    </div><!-- /.contact-timing -->
+                    <!-- ============================================================= INFORMATION : END ============================================================= -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+
+    </div><!-- /.links-social -->
